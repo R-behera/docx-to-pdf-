9a71e7a3e388d33d45f5d7422018ae71c094feb1
R-behera <rajbehera445@gmail.com>
2026-03-23 11:34:32 -0400

    Replace hardcoded API key with environment variable

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/convert', async (req, res) => {
     for (var i = 0; i < 30; i++) {
       await new Promise(function(r) { setTimeout(r, 3000); });
       const statusRes = await fetch('https://api.cloudconvert.com/v2/jobs/' + jobId, {
-        headers: { 'Authorization': 'Bearer ' + eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZGVhMGVlMjVjNzBmMDUwOTkzMjA0MjU2OWU3YmI2ZDUyOWJhMGJlZjZhMDQ4YTc1YTRiYmY1MmRhZjdlYzg2ZjhhNGQwZGZmZjI2ODU2ODMiLCJpYXQiOjE3NzM5NzM3MzMuNjUzODk1LCJuYmYiOjE3NzM5NzM3MzMuNjUzODk2LCJleHAiOjQ5Mjk2NDczMzMuNjQ3MjI2LCJzdWIiOiI3NDc4MDE0MCIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.JojVtIo36sw8zl6ssB7YV0YYTjWMr-h0bEcJ5l-uphYVUNQA7TBB09jktNgqBj2gzaLCF68NS2JjYtKiGGRA_WXAG4E_8-wr5iHSPQJBO-0IlDvLwgcu5O7OyL2Y4A8vNUXZu15iF6d_DXbpjIoAJNDWDGzpXfT7h3x2M7n1AsxvAQK7O-VUiyVfZcRIKeBHtrhTr5MxIJqBketKRdwa3lk_m0oOy1dTQ5yjy8SrpezEk4hey5cFKUIwEmJ2CCQhCFdY98df7eZWKk2r3xfWoxHAqXH_7Cuq1Zi6DF7pvvBYrCZOKk1M5M6ASzxRumUPhgwzX66bpY2AC8Ey2tg6GT-HaRc7HWBsK7d_S0GJSkBzoL_jmYOZIajW7G2m4eqBvmb0FbHH9FGg714dTqVAYyMJ0gC4BSq0nBPkGMDE1x5itP_19nt2slixtn7XY6Cz0rwIBWX9O_oP4l9nB9vx5fdg9-0Hoa8qpaupc7eZfw79i5KuTqCqdANekFTOkAJssR0IYMh8C2tA4be7R_5n10lSeMMtOBRtddWSaa2RenqxVW3m8ueLrLOMQwm9X7A20JYR0lPsJvXuwLbDUXLjCQgwaKJ7LXiVhJf_LU__Yp7_d1BeO1097K82ZEmcOq1f1PSGhGwsjP2pn32ZDbiaTopbvWNm7tI0IrFaZzdG9Jw }
+        headers: { 'Authorization': 'Bearer ' + CC_API_KEY}
       });
       const status = await statusRes.json();
       const exportTask = status.data.tasks.find(function(t) {
